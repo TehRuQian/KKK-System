28f8ff82e8b7ac6daedb00c69dded1c7b6cee2ba
Chua Jia Lin <148312909+ChuaJiaLin@users.noreply.github.com>
2025-01-14 10:56:17 +0800

    Add files via upload

---
+++ headermember.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>KKK Online System</title>
+    <link href="bootstrap.css">
+    <link href="../img/kkk_logo.png" rel="icon" type="/image/x-icon">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<style>
+  ul li:hover{
+    background-color: #5AB0FF;
+    border-radius: 10px;
+  }
+</style>
+<body>
+
+<nav class="navbar navbar-expand-lg bg-primary" data-bs-theme="dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="../member_main/member.php">
+        <img src="../img/kkk_logo_cropped.png" alt="kkk" style="height: 30px;">
+    </a>
+     
+    <button class="navbar-toggler ms-auto" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav ms-auto">
+        <li class="nav-item">
+          <a class="nav-link active ms-2" href="../member_main/member.php">Laman Utama</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active ms-2" href="../update_member_profile/profilmember.php">Profil</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active ms-2" href="#">Pinjaman</a>
+        </li>
+        <li>
+          <button onclick="window.location.href='../login.php';" class="btn btn-dark">Log Keluar</button>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+
