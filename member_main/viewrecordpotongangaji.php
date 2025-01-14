fe0b57620d3569bcef8b3baa1fe8b891383b75d8
Chua Jia Lin <148312909+ChuaJiaLin@users.noreply.github.com>
2025-01-14 10:59:31 +0800

    Update viewrecordpotongangaji.php

--- member_main/viewrecordpotongangaji.php
+++ member_main/viewrecordpotongangaji.php
@@ -61,7 +61,7 @@ if (!$result) {
     <p style="text-align: center; font-size:30px;"><b>Rekod Potongan Gaji</b></p>
 
     <nav>
-      <ul class="pagination ms-5 pagination-sm">
+      <ul class="dflex justify-content-center pagination ms-5 pagination-sm">
         <?php if($current_page>1): ?>
         <li class="page-item">
           <a class="page-link" href="?page=<?=$current_page-1;?>">&laquo</a>
