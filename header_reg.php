9bce8ddd49dbf963b2ec7721bca2ed996d3b2645
Yi Ya <tanyiya04@gmail.com>
2025-01-14 12:49:33 +0800

    Add files via upload

---
+++ header_reg.php
@@ -0,0 +1,26 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>KKK Pemohonan Anggota</title>
+    <link href="bootstrap.css" rel="stylesheet">
+    <link href="../img/kkk_logo.png" rel="icon" type="/image/x-icon">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+</head>
+<body>
+
+<nav class="navbar navbar-expand-lg" style="background-color: #5ab0ff;" data-bs-theme="light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+        <img src="../img/kkk_logo_cropped.png" alt="kkk" style="height: 30px;">
+    </a>
+    <a href="login.php" class="btn btn-dark d-flex">Log Keluar</a>
+  </div>
+</nav>
+
+
