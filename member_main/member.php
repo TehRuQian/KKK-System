259e4afd25f490d3ae1314b6811153ebd0d908e7
Chua Jia Lin <148312909+ChuaJiaLin@users.noreply.github.com>
2025-01-16 00:10:52 +0800

    Update member.php

--- member_main/member.php
+++ member_main/member.php
@@ -15,16 +15,39 @@ include '../headermember.php';
 include '../db_connect.php';
 $u_id = $_SESSION['funame'];
 
-$sql = "SELECT * FROM tb_financial
+$sql_saham = "SELECT * FROM tb_financial
         WHERE tb_financial.f_memberNo = '$u_id'";
 
-$result = mysqli_query($con, $sql);
+$result_saham = mysqli_query($con, $sql_saham);
 
-if (!$result) {
+if (!$result_saham) {
     die("Query failed: ".mysqli_error($con));
 }
 
-$row = mysqli_fetch_assoc($result);
+$row_saham = mysqli_fetch_assoc($result_saham);
+
+$sql_pinjaman= "SELECT
+  tb_ltype.lt_desc AS loanName,
+  COALESCE(SUM(tb_loan.l_loanPayable)) AS totalLoan
+  FROM tb_ltype
+    LEFT JOIN tb_loan 
+      ON tb_loan.l_loanType=tb_ltype.lt_lid
+      AND tb_loan.l_memberNo='$u_id'
+      AND tb_loan.l_status=3
+    GROUP BY tb_ltype.lt_desc
+    ORDER BY tb_ltype.lt_desc";
+
+$result_pinjaman = mysqli_query($con, $sql_pinjaman);
+
+if (!$result_pinjaman) {
+    die("Query failed: ".mysqli_error($con));
+}
+
+$loanTotals=[];
+while($row_pinjaman=mysqli_fetch_assoc($result_pinjaman)){
+  $loanTotals[$row_pinjaman['loanName']]=$row_pinjaman['totalLoan'];
+}
+
 ?>
 
 <style>
@@ -52,74 +75,52 @@ $row = mysqli_fetch_assoc($result);
           <tbody>
             <tr>
               <td scope="row">Modah Syer :</td>
-              <td>RM <?= $row['f_shareCapital'] ?? '0'; ?></td>
+              <td>RM <?= $row_saham['f_shareCapital'] ?? '0'; ?></td>
             </tr>
             <tr>
               <td scope="row">Modal Yuran :</td>
-              <td>RM <?= $row['f_feeCapital'] ?? '0'; ?></td>
+              <td>RM <?= $row_saham['f_feeCapital'] ?? '0'; ?></td>
             </tr>
             <tr>
               <td scope="row">Simpanan Tetap :</td>
-              <td>RM <?= $row['f_fixedSaving'] ?? '0'; ?></td>
+              <td>RM <?= $row_saham['f_fixedSaving'] ?? '0'; ?></td>
             </tr>
             <tr>
               <td scope="row">Tabung Anggota :</td>
-              <td>RM <?= $row['f_memberFund'] ?? '0'; ?></td>
+              <td>RM <?= $row_saham['f_memberFund'] ?? '0'; ?></td>
             </tr>
             <tr>
               <td scope="row">Simpanan Anggota :</td>
-              <td>RM <?= $row['f_memberSaving'] ?? '0'; ?></td>
+              <td>RM <?= $row_saham['f_memberSaving'] ?? '0'; ?></td>
             </tr>
           </tbody>
         </table>
       </div>
     </div>
 
-
-    <div class="card mb-3 col-10 my-5 mx-auto">
-      <div class="card-header text-white bg-primary d-flex justify-content-between align-items-center">
+<div class="card mb-3 col-10 my-5 mx-auto">
+    <div class="card-header text-white bg-primary d-flex justify-content-between align-items-center">
         Maklumat Pinjaman Ahli
-        <button type="button" class="btn btn-info"  onclick="window.location.href='viewrecordbayaranbalik.php'">
+        <button type="button" class="btn btn-info" onclick="window.location.href='viewrecordbayaranbalik.php'">
             Rekod Transaksi
-        </button> 
-      </div>
-      <div class="card-body">
-        <table class="table table-hover">
-          <tbody>
-            <tr>
-              <td scope="row">Al-Bai :</td>
-              <td>RM <?= $row['f_alBai'] ?? '0'; ?></td>
-            </tr>
-            <tr>
-              <td scope="row">Al-Innah :</td>
-              <td>RM <?= $row['f_alInnah'] ?? '0'; ?></td>
-            </tr>
-            <tr>
-              <td scope="row">B/Pulih Kenderaan :</td>
-              <td>RM <?= $row['f_bPulihKenderaan'] ?? '0'; ?></td>
-            </tr>
-            <tr>
-              <td scope="row">Road Tax & Insuran :</td>
-              <td>RM <?= $row['f_roadTaxInsurance'] ?? '0'; ?></td>
-            </tr>
-            <tr>
-              <td scope="row">Khas :</td>
-              <td>RM <?= $row['f_specialScheme'] ?? '0'; ?></td>
-            </tr>
-            <tr>
-              <td scope="row">Karnival Musim Istimewa :</td>
-              <td>RM <?= $row['f_specialSeasonCarnival'] ?? '0'; ?></td>
-            </tr>
-            <tr>
-              <td scope="row">Al-Qadrul Hassan :</td>
-              <td>RM <?= $row['f_alQadrulHassan'] ?? '0'; ?></td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
+        </button>
     </div>
-  <br><br><br>
-
+    <div class="card-body">
+      <table class="table table-hover">
+        <tbody>
+          <?php if(!empty($loanTotals)):?>
+          <?php foreach($loanTotals AS $loanName=>$totalLoan):?>
+          <tr>
+            <td scope="row"><?= $loanName; ?> :</td>
+            <td>RM <?= $totalLoan ?? 0; ?></td>
+          </tr>
+          <?php endforeach; ?>
+          <?php endif; ?>
+      </tbody>
+    </table>
+  </div>
+</div>
+<br><br><br>
 
 
 <?php include '../footer.php'; ?>
