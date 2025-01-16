098045710030033a5a3baa5b8c1e82554457efea
GOE JIE YING <148327232+jygoe@users.noreply.github.com>
2025-01-16 14:42:44 +0800

    Upload footer.php

---
+++ footer.php
@@ -0,0 +1,59 @@
+<footer class="bg-primary text-white mt-4">
+  <div class="container py-4 text-center">
+    <div class="row">
+      <!-- Address Section -->
+      <div class="col-md-4">
+        <h5 class="text-uppercase">Alamat</h5>
+        <ul class="list-unstyled">
+          <li><i class="fas fa-map-marker-alt me-2"></i>Peti Surat 127, </li>    
+          <li>Bandar Kota Bharu</li>
+          <li>15710 Kota Bharu</li>
+          <li>Kelantan</li>
+        </ul>
+      </div>
+
+      <!-- Contact Section -->
+      <div class="col-md-4 text-center">
+        <h5 class="text-uppercase">Hubungi Kami</h5>
+        <ul class="list-unstyled">
+          <li><i class="fas fa-phone-alt me-2"></i>Telefon: 09-1234567</li>
+          <li><i class="fas fa-envelope me-2"></i>Email: info@kada.gov.my</li>
+        </ul>
+        <!-- Social Media Section -->
+        <h5 class="text-uppercase mt-4 text-center">Ikuti Kami</h5>
+        <ul class="list-inline">
+          <li class="list-inline-item">
+            <a href="https://www.facebook.com/kadakemubu/?ref=embed_page" class="text-white" target="_blank">
+              <i class="fa-brands fa-facebook fs-4"></i>
+            </a>
+          </li>
+          <li class="list-inline-item">
+            <a href="http://www.kada.gov.my/" class="text-white" target="_blank">
+              <i class="fa fa-globe fs-4" aria-hidden="true"></i>
+            </a>
+          </li>
+          <li class="list-inline-item">
+            <a href="https://www.instagram.com/kadaofficial" class="text-white" target="_blank">
+              <i class="fa-brands fa-instagram fs-4"></i>
+            </a>
+          </li>
+        </ul>
+      </div>
+
+      <!-- Operation Hours Section -->
+      <div class="col-md-4 text-center">
+        <h5 class="text-uppercase">Waktu Operasi</h5>
+        <ul class="list-unstyled">
+          <li>Ahad - Rabu: 08:00 - 17:00</li>
+          <li>Khamis: 08:00 - 15:30</li>
+          <li>Jumaat: Rehat</li>
+          <li>Sabtu: Rehat</li>
+        </ul>
+      </div>
+    </div>
+    <hr class="border-light">
+    <div class="text-center">
+      <small>Hak Cipta © 2025 KADA. Semua Hak Terpelihara.</small>
+    </div>
+  </div>
+</footer>
