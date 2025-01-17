ecfa6cd39a7a526ae7cb4f17d1e765453cf34b92
Chua Jia Lin <148312909+ChuaJiaLin@users.noreply.github.com>
2025-01-17 13:34:52 +0800

    Update kemaskinikeluarga.php

--- update_member_profile/kemaskinikeluarga.php
+++ update_member_profile/kemaskinikeluarga.php
@@ -1,13 +1,11 @@
 <?php 
 include('../kkksession.php');
-if(!session_id())
-{
-  session_start();
+if (!session_id()) {
+    session_start();
 }
 
-if(isset($_SESSION['u_id']) != session_id())
-{
-  header('Location:../login.php'); 
+if (isset($_SESSION['u_id']) != session_id()) {
+    header('Location:../login.php'); 
 }
 
 include '../headermember.php';
@@ -15,206 +13,284 @@ include '../db_connect.php';
 $u_id = $_SESSION['funame'];
 
 function callResult($con, $u_id) {
+    $u_id = mysqli_real_escape_string($con, $u_id);
+    $sql = "SELECT tb_member.*, 
+                   tb_ugender.ug_desc AS gender, 
+                   tb_urace.ur_desc AS race, 
+                   tb_ureligion.ua_desc AS religion, 
+                   tb_umaritalstatus.um_desc AS maritalstatus, 
+                   tb_lbank.lb_desc AS bankname, 
+                   tb_loan.l_bankAccountNo AS bankaccount, 
+                   tb_homeState.st_desc AS homeState, 
+                   tb_officeState.st_desc AS officeState, 
+                   tb_member.m_memberApplicationID
+            FROM tb_member
+            LEFT JOIN tb_ugender ON tb_member.m_gender=tb_ugender.ug_gid
+            LEFT JOIN tb_urace ON tb_member.m_race=tb_urace.ur_rid
+            LEFT JOIN tb_ureligion ON tb_member.m_religion=tb_ureligion.ua_rid
+            LEFT JOIN tb_umaritalstatus ON tb_member.m_maritalStatus=tb_umaritalstatus.um_mid
+            LEFT JOIN tb_loan ON tb_member.m_memberNo = tb_loan.l_memberNo
+            LEFT JOIN tb_lbank ON tb_loan.l_bankName=tb_lbank.lb_id
+            LEFT JOIN tb_homeState ON tb_member.m_homeState=tb_homeState.st_id
+            LEFT JOIN tb_officeState ON tb_member.m_officeState=tb_officeState.st_id
+            WHERE tb_member.m_memberNo = '$u_id'";
 
-$sql = "SELECT tb_member.*,
-               tb_ugender.ug_desc AS gender,
-               tb_urace.ur_desc AS race,
-               tb_ureligion.ua_desc AS religion,
-               tb_umaritalstatus.um_desc AS maritalstatus,
-               tb_lbank.lb_desc AS bankname,
-               tb_loan.l_bankAccountNo AS bankaccount,
-               tb_homeState.st_desc AS homeState,
-               tb_officeState.st_desc AS officeState,
-               tb_member.m_memberApplicationID
-        FROM tb_member
-        LEFT JOIN tb_ugender ON tb_member.m_gender=tb_ugender.ug_gid
-        LEFT JOIN tb_urace ON tb_member.m_race=tb_urace.ur_rid
-        LEFT JOIN tb_ureligion ON tb_member.m_religion=tb_ureligion.ua_rid
-        LEFT JOIN tb_umaritalstatus ON tb_member.m_maritalStatus=tb_umaritalstatus.um_mid
-        LEFT JOIN tb_loan ON tb_member.m_memberNo = tb_loan.l_memberNo
-        LEFT JOIN tb_lbank ON tb_loan.l_bankName=tb_lbank.lb_id
-        LEFT JOIN tb_homeState ON tb_member.m_homeState=tb_homeState.st_id
-        LEFT JOIN tb_officeState ON tb_member.m_officeState=tb_officeState.st_id
-        WHERE tb_member.m_memberNo = '$u_id'";
+    return mysqli_query($con, $sql);
+}
 
-$result = mysqli_query($con, $sql);
+function getHeirs($con, $memberApplicationID) {
+    $memberApplicationID = mysqli_real_escape_string($con, $memberApplicationID);
+    $sql = "SELECT tb_heir.*, tb_hrelation.hr_desc AS heirrelation
+            FROM tb_heir 
+            LEFT JOIN tb_hrelation ON tb_heir.h_relationWithMember = tb_hrelation.hr_rid
+            WHERE h_memberApplicationID = '$memberApplicationID' 
+            ORDER BY h_heirID";
 
+    return mysqli_query($con, $sql);
+}
+
+function addOrUpdateHeir($con, $memberApplicationID, $heirID, $heirName, $heirIC, $heirRelation) {
+    $memberApplicationID = mysqli_real_escape_string($con, $memberApplicationID);
+    $heirName = mysqli_real_escape_string($con, $heirName);
+    $heirIC = mysqli_real_escape_string($con, $heirIC);
+    $heirRelation = mysqli_real_escape_string($con, $heirRelation);
+
+    if (!empty($heirID)) {
+        $sql = "UPDATE tb_heir SET h_name = '$heirName', h_ic = '$heirIC', h_relationWithMember = '$heirRelation'
+                WHERE h_heirID = '$heirID'";
+    } 
+
+    else {
+        $check_sql = "SELECT * FROM tb_heir WHERE h_memberApplicationID = '$memberApplicationID' AND h_ic = '$heirIC'";
+        $check_result = mysqli_query($con, $check_sql);
+        
+        if (mysqli_num_rows($check_result) == 0) {
+            $sql = "INSERT INTO tb_heir (h_memberApplicationID, h_name, h_ic, h_relationWithMember) 
+                    VALUES ('$memberApplicationID', '$heirName', '$heirIC', '$heirRelation')";
+        } 
+        
+        else {
+            return;
+        }
+    }
+
+    if (!mysqli_query($con, $sql)) {
+        //echo "Success!"
+    } 
+    
+    else {
+        //echo "Error: " . $sql . "<br>" . mysqli_error($con);
+    }
+}
+
+
+function deleteHeir($con, $heirId) {
+    $heirId = mysqli_real_escape_string($con, $heirId);
+    $sql = "DELETE FROM tb_heir WHERE h_heirID = '$heirId'";
+
+    if (mysqli_query($con, $sql)) { ?>
+        <script>
+            alert("Pewaris berjaya dipadam!");
+        </script>
+    <?php } 
+    
+    else {
+        echo "Error: " . $sql . "<br>" . mysqli_error($con);
+    }
+}
+
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if (isset($_POST['heirs'])) {
+        foreach ($_POST['heirs'] as $heir) {
+            $heirID = isset($heir['id']) ? $heir['id'] : '';
+            $name = $heir['name'];
+            $ic = $heir['ic'];
+            $relation = $heir['relation'];
+            addOrUpdateHeir($con, $_POST['memberApplicationID'], $heirID, $name, $ic, $relation);
+        }
+    }
+
+    if (isset($_POST['delete_heir'])) {
+        $heirId = $_POST['delete_heir'];
+        deleteHeir($con, $heirId);
+    }
+}
+
+
+$result = callResult($con, $u_id);
 if (!$result) {
     die("Query failed: ".mysqli_error($con));
 }
-
 $row = mysqli_fetch_assoc($result);
-
 $memberApplicationID = $row['m_memberApplicationID']; 
 
-$heir_query = "SELECT tb_heir.* ,
-                      tb_hrelation.hr_desc AS heirrelation
-                      FROM tb_heir 
-                      LEFT JOIN tb_hrelation ON tb_heir.h_relationWithMember = tb_hrelation.hr_rid
-                      WHERE h_memberApplicationID = '$memberApplicationID' ORDER BY h_heirID";
-
-$heir_result = mysqli_query($con, $heir_query);
-
+$heir_result = getHeirs($con, $memberApplicationID);
 if (!$heir_result) {
-  die("Query failed: ".mysqli_error($con));
-}
-
-return $heir_result;
-}
-
-$heir_result = callResult($con, $u_id);
-
-if($_SERVER['REQUEST_METHOD']==='POST' && isset($_POST['heir'])){
-  foreach ($_POST['heir'] as $heir){
-    $heirID = $heir['relation'] ? mysqli_real_escape_string($con,$heir['id']): '';
-    $name = $heir['relation'] ? mysqli_real_escape_string($con,$heir['name']): '';
-    $ic = $heir['relation'] ? mysqli_real_escape_string($con,$heir['ic']): '';
-    $relation = $heir['relation'] ?  mysqli_real_escape_string($con,$heir['relation']): '';
-
-
-if(!empty($_POST)) {
-    $update_query="UPDATE tb_heir
-                  SET 
-                    h_name='$name',
-                    h_ic='$ic',
-                    h_relationWithMember='$relation'
-                  WHERE h_heirID='$heirID'";
-
-    if(!mysqli_query($con, $update_query)) {
-      die("Update failed!" . mysqli_error($con));
-    }
-    else{
-      header('Location:profilmember.php');
-    }
-    }
-  }
-
+    die("Query failed: ".mysqli_error($con));
 }
 
 ?>
 
-  <style>
+<style>
     body {
-      background-color: #f9f9f9;
+        background-color: #f9f9f9;
     }
 
-    
-    .fixed-table-container{
-      position: fixed;
-      top: 60px;
-      width:100%;
-      background-color: White;
-      z-index:1;
-    }
-
-    .form-container{
-      max-width: 700px;
-      margin: 0 auto;
+    .form-container {
+        max-width: 700px;
+        margin: 0 auto;
     }
 
     .required {
-      color: red;
-      font-weight: bold;
+        color: red;
+        font-weight: bold;
     }
+</style>
 
-  </style>
-</head>
 <body>
-<!--
-  <table class="fixed-table-container">
-  <thead>
-    <tr>
-      <th scope="col" style="text-align: center; font-size:20px; padding: 20px;">Maklumat Keluarga dan Pewaris</th>
-    </tr>
-  </thead>
-</table>
--->
-  
-  <div class="my-3"></div>
-
-    <p style="text-align: center; font-size:30px;"><b>Maklumat Keluarga dan Pewaris</b></p>
-
-  <div class="my-3"></div>
-
-
-<form class="form-container" method="post" action="">
-  <fieldset>
-
-  <br><a style="color: red;">* Perhatian: Sekurang-kurangnya SATU pewaris diperlukan! *</a><br>
-  <a style="color: red;">* Perhatian: Untuk pewaris lebih, sila masukkan "-" untuk Nama dan No.KP / No. Srt Beranak, dan pilih "Tiada Hubungan" untuk Hubungan *</a>
-
-<?php
-$count = 1;
-  while ($heir_row = mysqli_fetch_assoc($heir_result)) {
-    if($count > 5) break; 
-    $heirID=$heir_row['h_heirID'];
-?>
-      <div class="my-3"></div>
-      <label class="form-label mt-4"><h5>Maklumat Pewaris <?= $count++; ?></h5></label>
-      <input type="hidden" name="heir[<?= $heirID ?>][id]" value="<?= $heirID ?>">
-      <div>
+    <form class="form-container" method="post" action="">
         <fieldset>
-          <label class="form-label">Nama <span class="required">*</span></label>
-            <input type="text" class="form-control" name="heir[<?= $heirID ?>][name]" value="<?= $heir_row['h_name']; ?>" required>
-        </fieldset>
-      </div>
-      <div class="my-3">
-        <fieldset>
-          <label class="form-label">No. KP / No. Srt Beranak <span class="required">*</span></label>
-            <input type="text" class="form-control" name="heir[<?= $heirID ?>][ic]" value="<?= $heir_row['h_ic']; ?>" required>
-        </fieldset>
-      </div>
-      <div>
-        <fieldset>
-          <label class="form-label">Hubungan <span class="required">*</span></label>
-          <select class="form-select" name="heir[<?= $heirID ?>][relation]" placeholder="<?= $heir_row['heirrelation']; ?>" required>
-            <option value="7" <?= $heir_row['h_relationWithMember'] == 7 ? 'selected' : ''; ?>>Tiada Hubungan</option>
-            <option value="1" <?= $heir_row['h_relationWithMember'] == 1 ? 'selected' : ''; ?>>Suami Isteri</option>
-            <option value="2" <?= $heir_row['h_relationWithMember'] == 2 ? 'selected' : ''; ?>>Anak</option>
-            <option value="3" <?= $heir_row['h_relationWithMember'] == 3 ? 'selected' : ''; ?>>Keturunan</option>
-            <option value="4" <?= $heir_row['h_relationWithMember'] == 4 ? 'selected' : ''; ?>>Orang Tua</option>
-            <option value="5" <?= $heir_row['h_relationWithMember'] == 5 ? 'selected' : ''; ?>>Saudara Kandung</option>
-            <option value="6" <?= $heir_row['h_relationWithMember'] == 6 ? 'selected' : ''; ?>>Lain-lain</option>
-          </select>
+            <br>
+            <h2 style="text-align: center;">Maklumat Keluarga dan Pewaris</h2>
+            
+            <?php 
+            $count=1;
+            while ($heir = mysqli_fetch_assoc($heir_result)){ ?>
 
+                <label class="form-label mt-4"><h5>Maklumat Pewaris <?= $count; ?></h5></label>
+                <input type="hidden" name="memberApplicationID" value="<?= $memberApplicationID ?>">
+                <input type="hidden" name="heirs[<?= $count ?>][id]" value="<?= $heir['h_heirID']; ?>">
+                <div>
+                    <label class="form-label">Nama <span class="required">*</span></label>
+                    <input type="text" class="form-control" name="heirs[<?= $count ?>][name]" value="<?= htmlspecialchars($heir['h_name'], ENT_QUOTES, 'UTF-8'); ?>" required>
+                </div>
+                <div class="my-3">
+                    <label class="form-label">No. KP / No. Srt Beranak <span class="required">*</span></label>
+                    <input type="text" class="form-control" name="heirs[<?= $count ?>][ic]" value="<?= htmlspecialchars($heir['h_ic'], ENT_QUOTES, 'UTF-8'); ?>" required>
+                </div>
+                <div>
+                    <label class="form-label">Hubungan <span class="required">*</span></label>
+                    <select class="form-select" name="heirs[<?= $count ?>][relation]" required>
+                        <option value="7" <?= $heir['h_relationWithMember'] == 7 ? 'selected' : ''; ?>>Tiada Hubungan</option>
+                        <option value="1" <?= $heir['h_relationWithMember'] == 1 ? 'selected' : ''; ?>>Suami Isteri</option>
+                        <option value="2" <?= $heir['h_relationWithMember'] == 2 ? 'selected' : ''; ?>>Anak</option>
+                        <option value="3" <?= $heir['h_relationWithMember'] == 3 ? 'selected' : ''; ?>>Keturunan</option>
+                        <option value="4" <?= $heir['h_relationWithMember'] == 4 ? 'selected' : ''; ?>>Orang Tua</option>
+                        <option value="5" <?= $heir['h_relationWithMember'] == 5 ? 'selected' : ''; ?>>Saudara Kandung</option>
+                        <option value="6" <?= $heir['h_relationWithMember'] == 6 ? 'selected' : ''; ?>>Lain-lain</option>
+                    </select>
+                </div>
+                <div class="d-flex justify-content-center">
+                    <button type="submit" class="btn btn-danger mt-4" onclick="confirmDelete(<?= $heir['h_heirID']; ?>)">Padam Pewaris</button>
+                </div>
+            <?php $count++; } ?>
+
+            <div id="new-heirs-container"></div>
+
+            <div class="d-flex justify-content-center">
+                <button type="button" class="btn btn-success mt-4" id="add_heir">Tambah Pewaris</button>
+            </div>
+            
+            <div class="d-flex justify-content-center">
+                <button onclick="confirmation(event)" class="btn btn-primary mt-4">Simpan</button>
+            </div>
         </fieldset>
-      </div>
-<?php 
-    }
-?>
+    </form>
 
     <div class="d-flex justify-content-center">
-      <button onclick="confirmation(event)" class="btn btn-primary mt-4">Simpan</button>
+        <button onclick="confirmationKembali()" class="btn btn-primary mt-4">Kembali</button>
     </div>
-  </fieldset>
-</form>
-        
-<script>
-  function confirmation(event){
-    const fields = document.querySelectorAll("[required]");
-    for (let field of fields) {
-      if (field.value.trim() === "") {
-        alert("Sila isi semua medan yang diperlukan.");
-        event.preventDefault();
-        return false;
-      }
-    }
-    let msg="Adakah anda pasti ingin mengemaskini maklumat anda?";
-    if(confirm(msg)==true){
-      alert("Tahniah! Maklumat anda telah berjaya dikemaskini!");
-    }
     
-    else{
-      event.preventDefault();
-      alert("Maklumat anda tidak dikemaskini.");
-      window.location.href="profilmember.php";
-    }
-  }
-</script>
 
-<div class="my-5"></div><br>
-  
+    <script>
+        function confirmationKembali(){
+            let msg = "Adakah anda pasti ingin kembali ke Profil? Perubahan anda tidak akan disimpan.";
+                if (confirm(msg) == true) {
+                    alert("Perubahan anda tidak disimpan!");
+                    window.location.href = "profilmember.php";
+                }
+        }
+          
+
+        function confirmDelete(heirId) {
+            const confirmation = confirm("Adakah anda pasti ingin memadam pewaris ini?");
+            if (confirmation) {
+                const deleteField = document.createElement("input");
+                deleteField.type = "hidden";
+                deleteField.name = "delete_heir";
+                deleteField.value = heirId;
+
+                const form = document.querySelector("form");
+                form.appendChild(deleteField);
+                form.submit();
+            }
+        }
+
+
+        function removeHeir(button) {
+            const heirDiv = button.closest('div').parentNode;
+            heirDiv.remove();
+            heirCount--;
+        }
+
+    let heirCount = <?= $count; ?>;
+
+    document.getElementById('add_heir').addEventListener('click', function() {
+        let heirDiv = document.createElement('div');
+        heirDiv.innerHTML = `
+            <label class="form-label mt-4"><h5>Maklumat Pewaris Baru</h5></label>
+            <div>
+                <label class="form-label">Nama <span class="required">*</span></label>
+                <input type="text" class="form-control" name="heirs[${heirCount +1}][name]" required>
+            </div>
+            <div class="my-3">
+                <label class="form-label">No. KP / No. Srt Beranak <span class="required">*</span></label>
+                <input type="text" class="form-control" name="heirs[${heirCount +1}][ic]" required>
+            </div>
+            <div>
+                <label class="form-label">Hubungan <span class="required">*</span></label>
+                <select class="form-select" name="heirs[${heirCount +1}][relation]" required>
+                    <option value="7">Tiada Hubungan</option>
+                    <option value="1">Suami Isteri</option>
+                    <option value="2">Anak</option>
+                    <option value="3">Keturunan</option>
+                    <option value="4">Orang Tua</option>
+                    <option value="5">Saudara Kandung</option>
+                    <option value="6">Lain-lain</option>
+                </select>
+            </div><br>
+            <div class="d-flex justify-content-center">
+                <button type="button" class="btn btn-danger" onclick="removeHeir(this)">Padam Pewaris</button>
+            </div>
+        `;
+
+        document.getElementById('new-heirs-container').appendChild(heirDiv);
+        heirCount++;
+    });
+
+
+        function confirmation(event) {
+            const fields = document.querySelectorAll("[required]");
+            for (let field of fields) {
+                if (field.value.trim() === "") {
+                    alert("Sila isi semua medan yang diperlukan.");
+                    event.preventDefault();
+                    return false;
+                }
+            }
+            let msg = "Adakah anda pasti ingin mengemaskini maklumat anda?";
+            if (confirm(msg) == true) {
+                alert("Tahniah! Maklumat anda telah berjaya dikemaskini!");
+                window.location.href = "profilmember.php";
+            } else {
+                event.preventDefault();
+                alert("Maklumat anda tidak dikemaskini.");
+                window.location.href = "profilmember.php";
+            }
+        }
+    </script>
 </body>
 </html>
 
-<?php include '../footer.php';?>
+<?php include '../footer.php'; ?>
