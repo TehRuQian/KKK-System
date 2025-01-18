ce2a511c3ce3a39d68627a08d7dbe79ba73325d8
Yuylam <yuylam113@gmail.com>
2025-01-18 19:55:32 +0800

    transaksi patch

--- transaksi/potongan_gaji.php
+++ transaksi/potongan_gaji.php
@@ -8,8 +8,19 @@
     FROM tb_financial
     INNER JOIN tb_member
     ON tb_financial.f_memberNo=tb_member.m_memberNo;";
-  $result = mysqli_query($con, $sql);
+  $result_financial = mysqli_query($con, $sql);
 
+  $sql_loan = "SELECT l_memberNo, l_loanType, COALESCE(SUM(l_loanPayable), 0) AS totalLoan
+             FROM tb_loan
+             WHERE l_status = 3
+             GROUP BY l_memberNo, l_loanType
+             ORDER BY l_memberNo, l_loanType;";
+  $result_loan = mysqli_query($con, $sql_loan);
+
+  $loanData = [];
+  while ($row_loan = mysqli_fetch_array($result_loan)) {
+      $loanData[$row_loan['l_memberNo']][$row_loan['l_loanType']] = $row_loan['totalLoan'];
+  }
 ?>
 
 <style>
@@ -69,36 +80,38 @@
             <th scope="col">Simpanan Tetap</th>
             <th scope="col">Tabung Anggota</th>
             <th scope="col">Simpanan Anggota</th>
-            <th scope="col">Al-Bai</th>
-            <th scope="col">Al-Innah</th>
-            <th scope="col">B/Pulih Kenderaan</th>
-            <th scope="col">Road Tax & Insuran</th>
-            <th scope="col">Khas</th>
-            <th scope="col">Karnival Musim Istimewa</th>
-            <th scope="col">Al-Qadrul Hassan</th>
+            <?php 
+              $sql = "SELECT * FROM tb_ltype
+                      ORDER BY lt_lid;";
+              $result_loanType = mysqli_query($con, $sql);
+              $loanTypes = [];
+              while ($row = mysqli_fetch_array($result_loanType)){
+                $loanTypes[] = $row['lt_lid'];
+                echo "<th scope='col'>" . $row['lt_desc'] . "</th>";
+              }
+            ?>
             <th scope="col">Dikemaskini</th>
           </tr>
         </thead>
         <tbody>
           <?php
-            while($row = mysqli_fetch_array($result)){
-                echo "<tr>";
-                    // Add checkbox for each row
-                    echo "<td><input type='checkbox' name='selected_members[]' value='" . $row['f_memberNo'] . "'></td>";
-                    echo "<td>" . $row['f_memberNo'] . "</td>";
-                    echo "<td>" . $row['m_name'] . "</td>";
+            while($row = mysqli_fetch_array($result_financial)){
+              $memberNo = $row['f_memberNo'];
+                echo "<tr onclick='toggleCheckbox(this)' data-member-id='" . $memberNo . "'>";
+                    echo "<td><input type='checkbox' name='selected_members[]' value='" . $memberNo . "'></td>";
+                    echo "<td>" . $memberNo . "</td>";
+                    echo "<td>" . htmlspecialchars($row['m_name']) . "</td>";
                     echo "<td>" . number_format($row['f_shareCapital'], 2) . "</td>";
                     echo "<td>" . number_format($row['f_feeCapital'], 2) . "</td>";
                     echo "<td>" . number_format($row['f_fixedSaving'], 2) . "</td>";
                     echo "<td>" . number_format($row['f_memberFund'], 2) . "</td>";
                     echo "<td>" . number_format($row['f_memberSaving'], 2) . "</td>";
-                    echo "<td>" . number_format($row['f_alBai'], 2) . "</td>";
-                    echo "<td>" . number_format($row['f_alInnah'], 2) . "</td>";
-                    echo "<td>" . number_format($row['f_bPulihKenderaan'], 2) . "</td>";
-                    echo "<td>" . number_format($row['f_roadTaxInsurance'], 2) . "</td>";
-                    echo "<td>" . number_format($row['f_specialScheme'], 2) . "</td>";
-                    echo "<td>" . number_format($row['f_specialSeasonCarnival'], 2) . "</td>";
-                    echo "<td>" . number_format($row['f_alQadrulHassan'], 2) . "</td>";
+
+                    foreach ($loanTypes as $loanType) {
+                        $totalLoan = isset($loanData[$memberNo][$loanType]) ? $loanData[$memberNo][$loanType] : 0;
+                        echo "<td>" . number_format($totalLoan, 2) . "</td>";
+                    }
+
                     echo "<td>" . date('d-m-Y', strtotime($row['f_dateUpdated'])) . "</td>";
                 echo "</tr>";
             }
@@ -106,7 +119,6 @@
         </tbody>
       </table>
 
-      <!-- Batch Action Buttons -->
       <div class="d-flex justify-content-center">
         <button type="button" class="btn btn-primary mx-2" onclick="submitForm('potongan_gaji_pengesahan.php')">Potongan Gaji</button>
       </div>
@@ -116,6 +128,11 @@
 
 <!-- JavaScript to handle "Select All" checkbox -->
 <script>
+  function toggleCheckbox(row) {
+    var checkbox = row.querySelector("input[type='checkbox']");
+    checkbox.checked = !checkbox.checked;
+  } 
+
   function toggleSelectAll() {
     var checkboxes = document.getElementsByName('selected_members[]');
     var isChecked = document.getElementById('select_all').checked;
--- transaksi/potongan_gaji_pengesahan.php
+++ transaksi/potongan_gaji_pengesahan.php
@@ -10,7 +10,7 @@
   } else {
       echo "
         <script>
-            alert ('No members selected.');
+            alert ('Sila pilih sekurang-kurangnya satu anggota.');
             window.location.href = 'transaksi.php';
         </script>";
   }
@@ -47,35 +47,32 @@
         <tr>
           <th scope="col">No Ahli</th>
           <th scope="col">Nama</th>
-          <th scope="col">Status Semasa</th>
-          <th scope="col">Status Baru</th>
+          <th scope="col">Perubahan Status</th>
           <th scope="col">Tindakan</th>
         </tr>
       </thead>
       <tbody>
         <?php
           foreach ($selectedMembers as $memberNo){
-            $sql = "SELECT * FROM tb_financial WHERE f_memberNo = $memberNo;";
-            $result = mysqli_query($con, $sql);
-            $financial = mysqli_fetch_assoc($result);
+            $sql_financial = "SELECT * FROM tb_financial WHERE f_memberNo = $memberNo;";
+            $result_financial = mysqli_query($con, $sql_financial);
+            $financial = mysqli_fetch_assoc($result_financial);
 
-            $sql = "SELECT m_name FROM tb_member WHERE m_memberNo = $memberNo;";
-            $result = mysqli_query($con, $sql);
-            $name = mysqli_fetch_assoc($result)['m_name'];
+            $sql_member = "SELECT * FROM tb_member WHERE m_memberNo = $memberNo;";
+            $result_member = mysqli_query($con, $sql_member);
+            $member = mysqli_fetch_assoc($result_member);
 
-            $sql = "SELECT * FROM tb_loan 
-                    WHERE l_memberNo = $memberNo AND l_status = 3;";
+            $sql_loan = "SELECT tb_loan.*, tb_ltype.lt_desc
+                         FROM tb_loan 
+                         JOIN tb_ltype ON tb_loan.l_loanType = tb_ltype.lt_lid
+                         WHERE tb_loan.l_memberNo = $memberNo AND tb_loan.l_status = 3;";
+            $result_loan = mysqli_query($con, $sql_loan);
 
             $newShareCapital = $financial['f_shareCapital'];
-            $newMemberFund = $financial['f_memberFund'];
+            $newFeeCapital = $financial['f_feeCapital'];
             $newFixedSaving = $financial['f_fixedSaving'];
-            $newAlBai = $financial['f_alBai'];
-            $newAlInnah = $financial['f_alInnah'];
-            $newBPulihKenderaan = $financial['f_bPulihKenderaan'];
-            $newRoadTaxInsurance = $financial['f_roadTaxInsurance'];
-            $newSpecialScheme = $financial['f_specialScheme'];
-            $newSpecialSeasonCarnival = $financial['f_specialSeasonCarnival'];
-            $newAlQadrulHassan = $financial['f_alQadrulHassan'];
+            $newMemberFund = $financial['f_memberFund'];
+            $newMemberSaving = $financial['f_memberSaving'];
             
             if ($financial['f_shareCapital'] < $minShareCapital) {
               $newShareCapital += $salaryDeductionForSaving;
@@ -90,32 +87,86 @@
               $newFixedSaving += $salaryDeductionForSaving;
             }
 
-            // if ($newAlBai != 0){
-            //   if($newAlBai - )
-            // }
+            $totalAmount = $salaryDeductionForMemberFund + $salaryDeductionForSaving;
 
             echo "<tr id='member_{$memberNo}'>";
                 echo "<td>" . $financial['f_memberNo'] . "</td>";
-                echo "<td>" . $name . "</td>";
+                echo "<td>" . $member['m_name'] . "</td>";
                 echo "<td>
-                        Modah Syer: " . number_format($financial['f_shareCapital'], 2) . "<br>
-                        Tabung Anggota: " . number_format($financial['f_memberFund'], 2) . "<br>
-                        Simpanan Anggota: " . number_format($financial['f_fixedSaving'], 2) . "
-                      </td>";
-                echo "<td>
-                        Modah Syer: " . number_format($newShareCapital, 2) . "<br>
-                        Tabung Anggota: " . number_format($newMemberFund, 2) . "<br>
-                        Simpanan Anggota: " . number_format($newFixedSaving, 2) . "
-                      </td>";
+                        <table class='table table-hover'>
+                          <tr>
+                            <th>Perkara</th>
+                            <th>Status Semasa</th>
+                            <th>Perubahan</th>
+                            <th>Status Baharu</th>
+                          </tr>";
+                          if($newShareCapital - $financial['f_shareCapital'] != 0){
+                            echo "
+                                <tr>
+                                <td>Modah Syer</td>
+                                <td>" . number_format($financial['f_shareCapital'], 2) . "</td>
+                                <td>" . number_format($newShareCapital - $financial['f_shareCapital'], 2) . "</td>
+                                <td>" . number_format($newShareCapital, 2) . "</td>
+                            </tr>";
+                          };
+                          if($newMemberFund - $financial['f_memberFund'] != 0){
+                            echo "
+                                <tr>
+                                <td>Tabung Anggota</td>
+                                <td>" . number_format($financial['f_memberFund'], 2) . "</td>
+                                <td>" . number_format($newMemberFund - $financial['f_memberFund'], 2) . "</td>
+                                <td>" . number_format($newMemberFund, 2) . "</td>
+                            </tr>";
+                          }
+                          if($newFixedSaving - $financial['f_fixedSaving'] != 0){
+                            echo "
+                                <tr>
+                                <td>Simpanan Anggota</td>
+                                <td>" . number_format($financial['f_fixedSaving'], 2) . "</td>
+                                <td>" . number_format($newFixedSaving - $financial['f_fixedSaving'], 2) . "</td>
+                                <td>" . number_format($newFixedSaving, 2) . "</td>
+                            </tr>";
+                          }
+                    echo"</table>";
+                    if(mysqli_num_rows($result_loan) > 0){
+                        echo "<table class='table table-hover'>
+                                <tr>
+                                    <th>ID</th>
+                                    <th>Pinjaman</th>
+                                    <th>Tangukkan Semasa</th>
+                                    <th>Bayaran</th>
+                                    <th>Tangukkan Baharu</th>
+                                </tr>";
+                        while($row = mysqli_fetch_assoc($result_loan)){
+                            $difference = $row['l_monthlyInstalment'];
+                            if ($difference > $row['l_loanPayable']){
+                                $difference = $row['l_loanPayable'];
+                            }
+                            $newLoanPayable = $row['l_loanPayable'] - $difference;
+                            $totalAmount += $difference;
+                            echo "<tr>";
+                                echo "<td>" . $row['l_loanApplicationID'] . "</td>";
+                                echo "<td>" . $row['lt_desc'] . "</td>";
+                                echo "<td>" . number_format($row['l_loanPayable'], 2) . "</td>";
+                                echo "<td>" . number_format($difference, 2) . "</td>";
+                                echo "<td>" . number_format($newLoanPayable, 2) . "</td>";
+                            echo "</tr>";
+                        }
+                        echo"</table>";
+                    }
+                echo "Jumlah Potongan Gaji: RM " . number_format($totalAmount, 2);
+                echo "</td>";
                 echo "<td><button type='button' class='btn btn-warning' onclick='removeMember(" . $financial['f_memberNo'] . ")'>Keluarkan</button></td>";
-                echo "</tr>";
+            echo "</tr>";
           }
         ?>
       </tbody>
     </table>
-
-    <button type="submit" class="btn btn-primary">Hantar</button>
+    <div class="d-flex justify-content-center">
+        <button type="submit" class="btn btn-primary">Hantar</button>
+    </div>
   </form>
+  <br>
 </div>
 
 <script>
@@ -125,16 +176,11 @@
 
     var selectedMembersInput = document.getElementById('selected_members');
     var currentSelection = selectedMembersInput.value.split(',');
-
-    // Remove the memberNo from the array
     var newSelection = currentSelection.filter(function(item) {
       return item !== memberNo.toString();
     });
 
-    // Update the hidden input with the new selected members
     selectedMembersInput.value = newSelection.join(',');
 
-    // Debugging: log the new value of selected_members
-    console.log("Updated selected_members:", selectedMembersInput.value);
   }
 </script>
